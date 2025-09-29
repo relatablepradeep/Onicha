746a08600ef1de876cec469c3872a1df4b28599e
pradeep <relatablepradeep@gmail.com>
2025-09-29 10:39:03 +0530

    added background strtcure

---
+++ src/app/Component/Background/Background.tsx
@@ -0,0 +1,17 @@
+export default function Background() {
+  return (
+    <>
+      <div className="w-48 h-[79vh]  bg-gray-300 "></div>
+          <div className="w-0 h-0 border-l-[190px] border-b-[190px]  border-l-white border-b-gray-300 border-2 rotate-[180deg]"></div>
+           
+
+
+
+    
+
+
+
+
+    </>
+  );
+}
