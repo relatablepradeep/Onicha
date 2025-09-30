a88653c5ebf413f0af4fe6a00cadd3bad84eae38
pradeep <relatablepradeep@gmail.com>
2025-09-30 09:42:12 +0530

    adde backrgound strtcure

--- src/app/Component/Background/Background.tsx
+++ src/app/Component/Background/Background.tsx
@@ -1,17 +1,15 @@
 export default function Background() {
   return (
     <>
-      <div className="w-48 h-[79vh]  bg-gray-300 "></div>
-          <div className="w-0 h-0 border-l-[190px] border-b-[190px]  border-l-white border-b-gray-300 border-2 rotate-[180deg]"></div>
-           
+      <div className="w-48 h-[79vh] bg-gray-300"></div>
 
+     
+      <div className="flex items-start  ">
+        
+        <div className="w-0 h-0 border-l-[190px] border-b-[190px] border-l-black border-b-gray-300 border-2 rotate-[180deg]"></div>
 
-
-    
-
-
-
-
+        <div className="w-[195vh] h-48 bg-black"></div>
+      </div>
     </>
   );
 }
